96dbd226e1c94eb6a6263f674f3b2fab4ecfb4d9
weixun <878826538@qq.com>
2023-12-10 22:05:55 +0800

    controller测试项目启动

--- main/src/main/java/com/study/main/MainApplication.java
+++ main/src/main/java/com/study/main/MainApplication.java
@@ -3,8 +3,11 @@ package com.study.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@ComponentScan("com.study")
 public class MainApplication {
 
     public static void main(String[] args) {
